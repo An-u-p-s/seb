2d3470fb16db4d21798df5e70d4ac5bc13fd192d
N.L.N.Rao <nellutlalnrao@gmail.com>
2019-09-02 02:08:39 +0530

    updated files

---
+++ nasdaq/website/home_default.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Nasdaq</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+/* Core Skeleton CSS ::: Start */
+body { font-size:12px; }
+.mtop15p { margin-top:15px; }
+.mbot15p { margin-bottom:15px; }
+.mbot20p { margin-bottom:20px; }
+.font-blue { color:#0091ba; }
+.pad0 { padding:0px; }
+.btn { font-family:'Trebuchet MS',Helvetica,sans-serif;font-size:12px;background-color:#0091ba;border-color:#0091ba;border-radius:0px; }
+/* Core Skeleton CSS ::: End */
+
+/* Header CSS ::: Start */
+.navbar { border-radius:0px;margin-bottom:0px; }
+.navbar-default { background-color:#fff; }
+.navbar-brand { padding:11px 11px; }
+.seb-logo-home { width:90px;height:auto; }
+/* Header CSS ::: End */
+
+/* Page CSS ::: Start */
+.seb-name { font-family:'Trebuchet MS',Helvetica,sans-serif;background-color:#0091ba;color:#fff;font-size:14px; }
+/* Page CSS ::: End */
+</style>
+</head>
+<body>
+<div>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#"><img class="seb-logo-home" src="images/logo_home.png"/></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Page 1-1</a></li>
+            <li><a href="#">Page 1-2</a></li>
+            <li><a href="#">Page 1-3</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Page 2</a></li>
+        <li><a href="#">Page 3</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</div>
+<!-- -->
+<div class="list-group">
+<div align="center" class="list-group-item seb-name">
+  <b>NASDAQ - National Association of Securities Dealers Automated Quotations</b>
+</div>
+</div>
+<!-- -->
+
+<div class="container-fluid">
+<div class="row">
+<!-- -->
+<div class="col-md-9">
+
+</div>
+<!-- -->  
+<div class="col-md-3">
+
+<div class="list-group">
+<div class="list-group-item pad0">
+
+<div class="container-fluid mtop15p mbot20p">
+
+<div class="row">
+<div align="center" class="col-xs-12">
+<h5><b>Are you New to Stock Exchange Market?</b></h5>
+</div>
+</div>
+
+<div class="row">
+<div align="center" class="col-xs-12">
+<button class="btn btn-primary"><b>GO TO NASDAQ UNIVERSITY <i class="fa fa-arrow-right"></i></b></button>
+</div>
+</div>
+
+</div>
+
+</div>
+</div>
+
+</div>
+<!-- -->
+</div>
+</div>
+
+</body>
+</html>
---
+++ nasdaq/website/university_default.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Nasdaq</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<style>
+/* Core Skeleton CSS ::: Start */
+body { font-size:12px; }
+.font-blue { color:#0091ba; }
+.pad0 { padding:0px; }
+.btn { font-family:'Trebuchet MS',Helvetica,sans-serif;font-size:12px;background-color:#0091ba;border-color:#0091ba;border-radius:0px; }
+/* Core Skeleton CSS ::: End */
+
+/* Header CSS ::: Start */
+.navbar { border-radius:0px;margin-bottom:0px; }
+.navbar-default { background-color:#fff; }
+.navbar-brand { padding:11px 11px; }
+.seb-logo-university { width:175px;height:auto; }
+/* Header CSS ::: End */
+
+/* Page CSS ::: Start */
+.seb-name { font-family:'Trebuchet MS',Helvetica,sans-serif;background-color:#0091ba;color:#fff;font-size:14px; }
+/* Page CSS ::: End */
+</style>
+</head>
+<body>
+<div>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">
+	    <img class="seb-logo-university" src="images/logo_university.png"/></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Page 1-1</a></li>
+            <li><a href="#">Page 1-2</a></li>
+            <li><a href="#">Page 1-3</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Page 2</a></li>
+        <li><a href="#">Page 3</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</div>
+<!-- -->
+<div class="list-group">
+<div align="center" class="list-group-item seb-name">
+  <b>NASDAQ - National Association of Securities Dealers Automated Quotations</b>
+</div>
+</div>
+<!-- -->
+
+<div class="container-fluid">
+<div class="row">
+<!-- -->
+<div class="col-xs-8">
+
+</div>
+<!-- -->  
+<div class="col-xs-4">
+
+</div>
+<!-- -->
+</div>
+</div>
+
+</body>
+</html>
